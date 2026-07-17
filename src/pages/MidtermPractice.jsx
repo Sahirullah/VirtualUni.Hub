6c6e95db8075272f67cecd5ef276375e5e027b07
Sahirullah <sahirullah313@gmail.com>
2026-07-17 13:14:28 +0500

    update

---
+++ src/pages/MidtermPractice.jsx
@@ -0,0 +1,6 @@
+import { allMidtermData } from '../data/midtermData';
+import ExamPractice from './ExamPractice';
+
+// MidtermPractice is now handled by the unified ExamPractice page
+// This file just re-exports ExamPractice for routing purposes
+export default ExamPractice;
