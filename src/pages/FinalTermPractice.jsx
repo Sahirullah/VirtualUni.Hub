798d50444b6a07f567cecae1377b60bf7ed798a1
Sahirullah <sahirullah313@gmail.com>
2026-07-18 15:58:53 +0500

    updaating

--- src/pages/FinalTermPractice.jsx
+++
@@ -1,6 +0,0 @@
-import { finalTermData, finalTermDataPart2 } from '../data/finalTermData';
-import ExamPractice from './ExamPractice';
-
-// FinalTermPractice is now handled by the unified ExamPractice page
-// This file just re-exports ExamPractice for routing purposes
-export default ExamPractice;
