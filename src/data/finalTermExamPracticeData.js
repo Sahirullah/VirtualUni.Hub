e037df089e066da22df0baf09a84daad59459364
Sahirullah <sahirullah313@gmail.com>
2026-07-26 14:45:51 +0500

    updating

--- src/data/finalTermExamPracticeData.js
+++ src/data/finalTermExamPracticeData.js
@@ -327,7 +327,7 @@ export const finalTermExamPracticeData = [
   { "question": "To read that object back, ObjectInputStream and its method ____ will be used.", "options": ["readObject()"], "correct": 0 }
 ]},
 
-// CS606
+//CS606
   {
     id: 5,
     code: 'CS606',
